5f95a36aebf04cd3842879871d1efa2617e580fd
Matt Schreiber <EMAIL>
2014-01-22 16:59:41 -0500

    50_devel.sh changes

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -1,11 +1,14 @@
-## Update the spf13-vim distribution of
-## Vim configuration files and plugins
-#update_spf13-vim() {
-#    cd $HOME/.spf13-vim-3
-#    git pull || return 1
-#    vim +BundleInstall! +BundleClean +q
-#}
-#
-## If Vim exists, try to update spf13-vim.
-## Otherwise, install spf13-vim
-#exists vim && update_spf13-vim || curl http://j.mp/spf13-vim -L -o - | sh
+# Update the spf13-vim distribution of
+# Vim configuration files and plugins
+update_spf13-vim() {
+    spf13dir="$HOME/.spf13-vim-3"
+    [[ -e $spf13dir ]] || return 1
+    cd $spf13dir
+    [[ $(git pull) =~ "up-to-date" ]] && return 1
+    vim +BundleInstall! +BundleClean +q +q
+    return 0
+}
+
+# If Vim exists, try to update spf13-vim.
+# Otherwise, install spf13-vim
+exists vim && update_spf13-vim || curl http://j.mp/spf13-vim -L -o - | sh
