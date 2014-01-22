04822ca07018f1fc60192822bd79c0e81ed529a3
Matt Schreiber <EMAIL>
2014-01-21 23:27:22 -0500

    Commented out init/50_devel.sh for now

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -1,11 +1,11 @@
-# Update the spf13-vim distribution of
-# Vim configuration files and plugins
-update_spf13-vim() {
-    cd $HOME/.spf13-vim-3
-    git pull
-    vim +BundleInstall! +BundleClean +q
-}
-
-# If Vim exists, try to update spf13-vim.
-# Otherwise, install spf13-vim
-exists vim && update_spf13-vim || curl http://j.mp/spf13-vim -L -o - | sh
+## Update the spf13-vim distribution of
+## Vim configuration files and plugins
+#update_spf13-vim() {
+#    cd $HOME/.spf13-vim-3
+#    git pull || return 1
+#    vim +BundleInstall! +BundleClean +q
+#}
+#
+## If Vim exists, try to update spf13-vim.
+## Otherwise, install spf13-vim
+#exists vim && update_spf13-vim || curl http://j.mp/spf13-vim -L -o - | sh
