b47a32223da1e91809aa34e61e2d124cae96697a
Matt Schreiber <EMAIL>
2014-01-23 00:58:26 -0500

    Refining recursion

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -1,5 +1,12 @@
 # Update the spf13-vim distribution of
 # Vim configuration files and plugins
+#
+# N.B. For some reason this seems to break
+# spf13 if it's already installed.  For now,
+# updates are handled by reinstalling.  Because
+# of the way spf13 uses modular vimrc files, this
+# shouldn't cause any serious problems with existing
+# files.
 update_spf13-vim() {
     spf13dir="$HOME/.spf13-vim-3"
     [[ -e $spf13dir ]] || return 1
