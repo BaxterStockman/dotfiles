0a11bb64c17791d944fdfa8d40ae545873ddf16a
Matt Schreiber <EMAIL>
2014-01-23 13:19:21 -0500

    Refining recursion

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -12,7 +12,22 @@ update_spf13-vim() {
 
 # If Vim exists, try to update spf13-vim.
 # Otherwise, install spf13-vim
-exists vim && update_spf13-vim || exec curl -L http://j.mp/spf13-vim3 | sh
+if exists vim; then
+  cat <<EOF
+Would you like to update spf13-vim?
+
+This will be skipped if "Y" isn't pressed within the next 15 seconds.
+EOF
+  read -N 1 -t 15 -p "Update spf13-vim? [y/N] " update_spf13; echo
+  if [[ "$update_spf13" =~ [Yy] ]]; then
+    e_header "Updating spf13-vim"
+    update_spf13-vim || exec curl -L http://j.mp/spf13-vim3 | sh
+    echo "Updated spf13-vim." ||
+    echo "Error updating spf13-vim."
+  else
+    echo "Skipping."
+  fi
+fi
 
 if ! exists perlbrew; then
   cat <<EOF
