2ef16aee5005b351f3045abbdbcc4e548aaae14e
Matt Schreiber <EMAIL>
2014-01-23 01:28:09 -0500

    Refining recursion

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -18,4 +18,4 @@ update_spf13-vim() {
 
 # If Vim exists, try to update spf13-vim.
 # Otherwise, install spf13-vim
-exists vim && exec curl http://j.mp/spf13-vim -L -o - | sh
+exists vim && exec curl -L http://j.mp/spf13-vim | sh
