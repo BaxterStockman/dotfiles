b5492decf0e3e9ef613ea085c58443a6cde0344f
Matt Schreiber <EMAIL>
2015-06-21 13:56:17 -0400

    Revised usage

--- run/20_init.sh
+++ run/20_init.sh
@@ -11,3 +11,16 @@ run () {
     e_header "Sourcing ${srcfile}"
     source "$srcfile"
 }
+
+parseopts () {
+    local opt_name opt_value
+    while (( $# )); do
+        read opt_name opt_value _ <<<"$@"
+            case "$opt_name" in
+                -V|--skip-vim-plugins)
+                    DOTFILES_SKIP_VIM_PLUGINS=true
+                    shift
+                    ;;
+            esac
+    done
+}
